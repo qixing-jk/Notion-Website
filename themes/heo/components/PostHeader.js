0ca65c6c81f0f2c44d19cd2b89adc4f88be9c269
qi_xing_jk <qi_xing_jk@proton.me>
2024-01-19 20:22:04 +0800

    feat(使文章封面图清晰化): 修改其透明度并删除高斯模糊

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -31,7 +31,7 @@ export default function PostHeader({ post, siteInfo }) {
             <div style={{ backdropFilter: 'blur(15px)' }} className={'bg-[#0060e0] absolute top-0 w-full h-full py-10 flex justify-center items-center'}>
 
                 {/* 文章背景图 */}
-                <div id='post-cover-wrapper' style={{ filter: 'blur(15px)' }} className='coverdiv lg:translate-x-96 opacity-50 lg:rotate-12'>
+                <div id='post-cover-wrapper' className='coverdiv lg:translate-x-96 opacity-80 lg:rotate-12'>
                     <LazyImage id='post-cover' className='w-full h-full object-cover opacity-80 max-h-[50rem] min-w-[50vw] min-h-[20rem]' src={headerImage} />
                 </div>
 
