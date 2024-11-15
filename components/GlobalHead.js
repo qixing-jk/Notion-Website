76036fe7c49403b921a9f4aba0d305cd8b1fe57a
anime <street-anime-olive@duck.com>
2024-11-15 15:54:14 +0800

    fix(初次加载过程中出现undefined的问题):

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -277,7 +277,7 @@ const getSEOMeta = (props, router, locale) => {
       return {
         title: post
           ? `${post?.title} | ${siteInfo?.title}`
-          : `${siteInfo?.title} | loading`,
+          : `${siteInfo?.title || '加载中'} | loading`,
         description: post?.summary,
         type: post?.type,
         slug: post?.slug,
