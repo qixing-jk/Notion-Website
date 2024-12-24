e5f4beee26d2a4db57642e5eabe5b537399ac665
anime <street-anime-olive@duck.com>
2024-12-24 22:17:08 +0800

    Revert "feat(优化build打包): 根据是否启用按需动态导入"
    This reverts commit 50426c9da72a823326ee5b1ea7008376ed159ae4.

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,24 @@ import '@/styles/utility-patterns.css'
 // core styles shared by all of react-notion-x (required)
 import '@/styles/notion.css' //  重写部分notion样式
 import 'react-notion-x/src/styles.css' // 原版的react-notion-x
+
 import useAdjustStyle from '@/hooks/useAdjustStyle'
 import { GlobalContextProvider } from '@/lib/global'
 import { getBaseLayoutByTheme } from '@/themes/theme'
 import { useRouter } from 'next/router'
 import { useCallback, useMemo } from 'react'
-import { getQueryParam } from '@/lib/utils'
+import { getQueryParam } from '../lib/utils'
 
 // 各种扩展插件 这个要阻塞引入
 import BLOG from '@/blog.config'
 import ExternalPlugins from '@/components/ExternalPlugins'
 import SEO from '@/components/SEO'
+import { zhCN } from '@clerk/localizations'
 import dynamic from 'next/dynamic'
+// import { ClerkProvider } from '@clerk/nextjs'
+const ClerkProvider = dynamic(() =>
+  import('@clerk/nextjs').then(m => m.ClerkProvider)
+)
 
 /**
  * App挂载DOM 入口文件
@@ -46,13 +52,6 @@ const MyApp = ({ Component, pageProps }) => {
   )
 
   const enableClerk = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-  let ClerkProvider, zhCN
-  if (enableClerk) {
-    ClerkProvider = dynamic(() =>
-      import('@clerk/nextjs').then(m => m.ClerkProvider)
-    )
-    zhCN = dynamic(() => import('@clerk/localizations').then(m => m.zhCN))
-  }
   const content = (
     <GlobalContextProvider {...pageProps}>
       <GLayout {...pageProps}>
