a72589f6cace63e154f4e423308585ed9d25b252
anime <street-anime-olive@duck.com>
2024-12-26 23:26:48 +0800

    fix(修复header部分logo样式错误):

--- themes/heo/components/Logo.js
+++ themes/heo/components/Logo.js
@@ -16,7 +16,7 @@ const Logo = props => {
           className='mr-3 hidden md:block'
           priority
         />
-        <div id='logo-text' className='group rounded-2xl flex-none'>
+        <div id='logo-text' className='group rounded-2xl flex-none relative'>
           <div className='logo group-hover:opacity-0 opacity-100 visible group-hover:invisible text-lg my-auto rounded dark:border-white duration-200'>
             {siteConfig('TITLE')}
           </div>
