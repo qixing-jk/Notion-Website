7c7ca0de9bc44f9a606ddc42fc6890f50ba46024
anime <street-anime-olive@duck.com>
2024-12-26 23:43:06 +0800

    feat(调整HEO PostHeader标题部分样式):

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -95,7 +95,7 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
           </div>
 
           {/* 文章Title */}
-          <div className='max-w-6xl font-bold text-3xl lg:text-5xl md:leading-snug shadow-text-md flex  justify-center md:justify-start text-white'>
+          <div className='max-w-6xl font-bold text-3xl lg:text-4xl md:leading-snug shadow-text-md flex  justify-center md:justify-start text-white'>
             {siteConfig('POST_TITLE_ICON') && (
               <NotionIcon icon={post.pageIcon} priority />
             )}
