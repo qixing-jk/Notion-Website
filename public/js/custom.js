e594146cacf72b0aa566c9c4b71d66b4c14b21eb
anime <street-anime-olive@duck.com>
2024-12-30 03:14:31 +0800

    feat(改用setInterval检测是否加载完毕，并优化处理逻辑):

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,61 +1,66 @@
-function processSpoiler() {
-  const articleElement = document.querySelector(
-    '#article-wrapper #notion-article'
-  )
-  if (articleElement) {
-    setTimeout(() => {
-      processTextNodes(articleElement, 'spoiler-text')
-      observer.disconnect()
-    }, 3000)
+function convertTextToSpoilerSpan(node, className) {
+  const regex = /\[sp](.*?)\[\/sp]/g
+  const wholeText = node.wholeText
+  const fragments = []
+  let lastIndex = 0
+  let match
+  while ((match = regex.exec(wholeText)) !== null) {
+    console.log('符合要求的文字' + wholeText)
+    // 添加前面未匹配的部分
+    if (match.index > lastIndex) {
+      fragments.push(
+        document.createTextNode(wholeText.slice(lastIndex, match.index))
+      )
+    }
+
+    // 创建 span 包裹的内容
+    const span = document.createElement('span')
+    span.textContent = match[1] // 提取匹配的内容
+    if (className) {
+      span.className = className
+    }
+    fragments.push(span)
+    // 设置lastIndex
+    lastIndex = regex.lastIndex
+  }
+  if (fragments.length) {
+    // 添加剩余未匹配的部分
+    if (lastIndex < wholeText.length) {
+      fragments.push(document.createTextNode(wholeText.slice(lastIndex)))
+    }
+
+    // 替换原节点
+    fragments.forEach(fragment => {
+      node.parentNode.appendChild(fragment)
+    })
+    node.remove()
   }
 }
 
 function processTextNodes(root, className) {
   const walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null)
+  const waitProcessNodes = []
   while (walker.nextNode()) {
     const node = walker.currentNode
-    const wholeText = node.textContent // 获取文本内容
-
-    const fragments = []
-    let lastIndex = 0
-    let match
-    const regex = /\[sp](.*?)\[\/sp]/g
-    while ((match = regex.exec(wholeText)) !== null) {
-      console.log('符合要求的文字' + wholeText)
-      // 添加前面未匹配的部分
-      if (match.index > lastIndex) {
-        fragments.push(
-          document.createTextNode(wholeText.slice(lastIndex, match.index))
-        )
-      }
-
-      // 创建 span 包裹的内容
-      const span = document.createElement('span')
-      span.textContent = match[1] // 提取匹配的内容
-      if (className) {
-        span.className = className
-      }
-      fragments.push(span)
-      // 设置lastIndex
-      lastIndex = regex.lastIndex
-    }
-    if (fragments.length) {
-      // 添加剩余未匹配的部分
-      if (lastIndex < wholeText.length) {
-        fragments.push(document.createTextNode(wholeText.slice(lastIndex)))
-      }
-
-      // 替换原节点
-      fragments.forEach(fragment => {
-        console.log(node.parentNode.appendChild(fragment))
-      })
-      node.remove()
-    }
+    waitProcessNodes.push(node)
+  }
+  for (const waitProcessNode of waitProcessNodes) {
+    convertTextToSpoilerSpan(waitProcessNode, className)
   }
 }
 
-const observer = new MutationObserver(() => {
-  processSpoiler()
-})
+function textToSpoiler() {
+  const intervalID = setInterval(() => {
+    const articleElement = document.querySelector(
+      '#article-wrapper #notion-article main'
+    )
+    if (articleElement) {
+      setTimeout(() => {
+        processTextNodes(articleElement, 'spoiler-text')
+        clearInterval(intervalID)
+      }, 300)
+    }
+  }, 1000)
+}
 
-observer.observe(document.body, { childList: true, subtree: true })
+textToSpoiler()
