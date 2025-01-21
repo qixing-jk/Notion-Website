32f1272289416887625082bbd54e842f03014762
anime <street-anime-olive@duck.com>
2025-01-22 03:46:16 +0800

    feat(Tailwind screens 断点从配置文件中解耦，便于react中调用):

---
+++ tailwind.screens.js
@@ -0,0 +1,11 @@
+module.exports = {
+  sm: '540px',
+  // => @media (min-width: 576px) { ... }
+  md: '720px',
+  // => @media (min-width: 768px) { ... }
+  lg: '960px',
+  // => @media (min-width: 992px) { ... }
+  xl: '1140px',
+  // => @media (min-width: 1200px) { ... }
+  '2xl': '1536px'
+}
