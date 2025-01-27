1e40b004a4fa2765a15db244dbd4ad47bab46328
anime <street-anime-olive@duck.com>
2025-01-28 02:46:57 +0800

    ci(lighthouse 添加 lighthouse-plugin-field-performance 插件):

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,10 @@
 module.exports = {
   ci: {
+    collect: {
+      settings: {
+        plugins: ['lighthouse-plugin-field-performance']
+      }
+    },
     assert: {
       preset: 'lighthouse:recommended'
     }
