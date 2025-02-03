5bbd66d8ff5edc710df9aa15b4bd78fe8bf87316
anime <street-anime-olive@duck.com>
2025-02-03 20:01:49 +0800

    docs(动态加载 fontawesome icon):

--- components/FontAwesomeIcon.js
+++ components/FontAwesomeIcon.js
@@ -1,11 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
 
+
+/**
+ * Asynchronously imports and returns a FontAwesome icon based on the specified style and name.
+ *
+ * @param {string} style - The style prefix for the FontAwesome icon (e.g., 'fas', 'far', 'fab').
+ * @param {string} name - The name of the icon in kebab-case (e.g., 'file-word').
+ * @returns {Promise<object>} - A promise that resolves to the imported FontAwesome icon.
+ * @throws {Error} - Throws an error if the style prefix is invalid.
+ */
 const getIcon = async (style, name) => {
   const formattedName = name
     .replace(/-([a-z])/g, (_, char) => char.toUpperCase())
     .replace(/^\w/, c => c.toLowerCase()) // file-word -> fileWord
-
   let iconModule
   switch (style) {
     case 'fas':
@@ -23,10 +31,16 @@ const getIcon = async (style, name) => {
     default:
       throw new Error('Invalid style prefix')
   }
-
   return iconModule[formattedName]
 }
 
+/**
+ * A component that renders a FontAwesome icon asynchronously.
+ *
+ * @param {Object} props - Component props.
+ * @param {string} props.icon - The style prefix and name of the icon to render, separated by a space (e.g., 'fas address-card').
+ * @returns {JSX.Element} - The rendered FontAwesome icon, or null if the icon could not be loaded.
+ */
 export const DynamicFontAwesomeIcon = ({ icon }) => {
   const [iconComponent, setIconComponent] = useState(null)
 
